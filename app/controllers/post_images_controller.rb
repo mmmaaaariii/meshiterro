1a3107b9dec3cdb784759f1262194015685a6e80
mmmaaaariii <166019982+mmmaaaariii@users.noreply.github.com>
2024-06-02 07:19:14 +0000

    [FIN]バリデーション・権限を設定しよう

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,8 +8,11 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
   
   def index
@@ -26,6 +29,8 @@ class PostImagesController < ApplicationController
     post_image.destroy
     redirect_to post_images_path
   end
+  
+  
   
    # 投稿データのストロングパラメータ
   private
